67a324b9a62687564c1ef89d6d250c8bd08b6d73
alexlam184 <alexwork0211@gmail.com>
2023-05-03 00:32:22 +0800

    test grant permission

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [notiPerm, setNotiPerm] = useState(false);
 
   return (
     <>
@@ -26,6 +27,7 @@ function App() {
             //request notification permission
             Notification.requestPermission().then(function (getperm) {
               console.log("Perm granted", getperm);
+              setNotiPerm(true);
             });
           }}
         >
@@ -34,6 +36,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>noti perm grant: {notiPerm ? "true" : "false"}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
